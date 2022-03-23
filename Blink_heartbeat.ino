d87dd2422bda177fb7c0bdf193408ed44715ad38
emilygurr <97482488+emilygurr@users.noreply.github.com>
2022-03-23 17:16:02 -0600

    Add files via upload

---
+++ Blink_heartbeat.ino
@@ -0,0 +1,18 @@
+
+// the setup function runs once when you press reset or power the board
+
+void setup() {
+  pinMode(13, OUTPUT);
+}
+
+// the loop function runs over and over again forever
+void loop() {
+  digitalWrite(13, HIGH);   // turn the LED on (HIGH is the voltage level)
+  delay(250);                       // wait for a second
+  digitalWrite(13, LOW);    // turn the LED off by making the voltage LOW
+  delay(100);  
+  digitalWrite(13, HIGH);   // turn the LED on (HIGH is the voltage level)
+  delay(250);                       // wait for a second
+  digitalWrite(13, LOW);    // turn the LED off by making the voltage LOW
+  delay(800);// wait for a second
+}
